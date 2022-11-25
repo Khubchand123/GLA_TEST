d8b470d961db9450df238b412bf831a25d94b639
Khubchand <khubchandbansal2@gmail.com>
2022-11-25 23:12:04 +0530

    final

--- k.js
+++ k.js
@@ -13,3 +13,33 @@ function app() {
   });
 }
 document.onload = app();
+
+document.getElementById('sh').addEventListener("click",display);
+
+function display(){
+    var v=document.getElementById('search').value;
+    request(v);
+}
+var data;
+function request(s){
+    const url="https://api.tvmaze.com/search/shows?q=";
+    const xhr=new XMLHttpRequest;
+    xhr.onload=function(){
+        var c=document.getElementById('m');
+        data=JSON.parse(this.response);
+        data.forEach((d)=>{
+            console.log(d.show.image.original);
+            var im=d.show.image.original;
+            var div=document.createElement('div');
+        div.className='mov';
+        div.innerHTML=`<img src="${im}" alt="">`;
+        c.appendChild(div);
+        })
+    }
+    xhr.onerror=function(){
+        console.log('error');
+    }
+    xhr.open("GET",url+s);
+    xhr.send();
+    
+}
